ba91152a6cb6b8eba7b06a1ef646f9a294c045e5
Vabolos <vaboloslol@gmail.com>
2023-12-05 22:17:14 +0100

    added console logging to main plugin file

--- src/main/java/deathcount/deathcount/DeathCount.java
+++ src/main/java/deathcount/deathcount/DeathCount.java
@@ -1,5 +1,6 @@
 package deathcount.deathcount;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DeathCount extends JavaPlugin {
@@ -7,11 +8,12 @@ public final class DeathCount extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("DeathCount is running!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("DeathCount is shutting down!");
     }
 }
